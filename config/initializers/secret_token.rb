f750f859eb3cdb4a11e0dcf2f6387a6b3b68aceb
David Nguyen <nugen@debianVM>
2014-05-20 20:23:32 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapProject::Application.config.secret_token = '9c7dfb2a698450cacadf6a7fbafea1c3f6a7c252259f79e73fa9d6abb00b5f1e373fd02f34abee9477c99ad51afa651dc4e70238d484c577b19bd6faa359df41'
